536993836f3a9150f94f63c3eba2788b794bebb1
amygnolyhc <amygnolyhc@gmail.com>
2025-11-13 21:42:33 +1100

    Add files via upload
    test test

---
+++ script.js
@@ -0,0 +1,355 @@
+ const smiskiData = {
+  "Series 1": {
+    cover: "images/Series_1/peeking.PNG",
+    figures: [
+      { name: "Hugging Knees", img: "images/Series_1/hugging_knees.PNG"},
+      { name: "Sitting", img: "images/Series_1/sitting.PNG"},
+      { name: "Looking Back", img: "images/Series_1/looking_back.PNG" },
+      { name: "Lounging", img: "images/Series_1/lounging.PNG" },
+      { name: "Hiding", img: "images/Series_1/hiding.PNG" },
+      { name: "Peeking", img: "images/Series_1/peeking.PNG" },
+      { name: "Little Smiski Lifting", img: "ps://www.smiski.com/assets/img/series/living/sleeping.png"},
+      { name: "Little Smiski Peeking", img: "ps://www.smiski.com/assets/img/series/living/sleeping.png" },
+      { name: "Little Smiski Hugging", img: "ps://www.smiski.com/assets/img/series/living/secret.png" },
+      { name: "Secret Scream", img: "ps://www.smiski.com/assets/img/series/living/secret.png" },
+    ],
+  },
+  "Series 2": {
+    cover: "images/Series_2/pushing.PNG",
+    figures: [
+      { name: "Kneeling", img: "images/Series_2/kneeling.PNG" },
+      { name: "Climbing", img: "images/Series_2/climbing.PNG" },
+      { name: "Daydreaming", img: "images/Series_2/daydreaming.PNG" },
+      { name: "Pushing", img: "images/Series_2/pushing.PNG" },
+      { name: "Peeking", img: "images/Series_2/peeking.PNG" },
+      { name: "Listening", img: "images/Series_2/listening.PNG" },
+    ],
+  },
+  "Series 3": {
+    cover: "images/Series_3/handstand.PNG",
+    figures: [
+      { name: "Bridge", img: "images/Series_3/bridge.PNG" },
+      { name: "Peeking", img: "images/Series_3/peeking.PNG" },
+      { name: "Climbing", img: "images/Series_3/climbing.PNG" },
+      { name: "Little", img: "images/Series_3/little.PNG" },
+      { name: "Hiding", img: "images/Series_3/hiding.PNG"},
+      { name: "Handstand", img: "images/Series_3/handstand.PNG" },
+    ],
+  },
+  "Series 4": {
+    cover: "images/Series_4/sneaking.PNG",
+    figures: [
+      { name: "Sneaking", img: "images/Series_4/sneaking.PNG"},
+      { name: "Scared", img: "images/Series_4/scared.PNG" },
+      { name: "Relaxing", img: "images/Series_4/relaxing.PNG" },
+      { name: "Lazy", img: "images/Series_4/lazy.PNG" },
+      { name: "Stuck", img: "images/Series_4/stuck.PNG" },
+      { name: "Defeated", img: "images/Series_4/defeated.PNG" },
+    ],
+  },
+  "Bath Series": {
+    cover: "images/bath series/shampooing.PNG",
+    figures: [
+      { name: "Shampooing", img: "images/bath series/shampooing.PNG"},
+      { name: "Not Looking", img: "images/bath series/not-looking.PNG"},
+      { name: "Scrubbing", img: "images/bath series/scrubbing.PNG"},
+      { name: "With Duck", img: "images/bath series/with-duck.PNG"},
+      { name: "Dazed", img: "images/bath series/dazed.PNG"},
+      { name: "Looking", img: "images/bath series/looking.PNG"},
+    ],
+  },
+  "Toilet Series": {
+    cover: "images/toilet series/squatting.PNG",
+    figures: [
+      { name: "Peek-A-Boo", img: "images/toilet series/peek-a-boo.PNG"},
+      { name: "Little (Smelly)", img: "images/toilet series/little-smelly.PNG"},
+      { name: "Squatting", img: "images/toilet series/squatting.PNG"},
+      { name: "Helping Out", img: "images/toilet series/helping-out.PNG"},
+      { name: "Resting", img: "images/toilet series/resting.PNG"},
+      { name: "Holding In", img: "images/toilet series/holding-in.PNG"},
+    ],
+  },
+  "Living Series": {
+    cover: "images/living series/daydreaming.PNG" ,
+    figures: [
+      { name: "Daydreaming", img: "images/living series/daydreaming.PNG"},
+      { name: "Playing", img: "images/living series/playing.PNG"},
+      { name: "Hiding", img: "images/living series/hiding.PNG"},
+      { name: "Nap Time", img: "images/living series/nap-time.PNG"},
+      { name: "Thinking", img: "images/living series/thinking.PNG"},
+      { name: "Lifting", img: "images/living series/lifting.PNG"},
+    ],
+  },
+  "Bed Series": {
+    cover: "images/bed series/before-rest.PNG",
+    figures: [
+      { name: "Before Rest", img: "images/bed series/before-rest.PNG"},
+      { name: "Sleepy", img: "images/bed series/sleepy.PNG"},
+      { name: "Co-Sleeping", img: "images/bed series/co-sleeping.PNG"},
+      { name: "Reading", img: "images/bed series/reading.PNG"},
+      { name: "Good Night", img: "images/bed series/goodnight.PNG"},
+      { name: "Crying", img: "images/bed series/crying.PNG"},
+    ],
+  },
+  "Yoga Series": {
+    cover: "images/yoga series/lotus-pose.PNG",
+    figures: [
+      { name: "Lotus Pose", img: "images/yoga series/lotus-pose.PNG"},
+      { name: "Twist Pose", img: "images/yoga series/twist-pose.PNG"},
+      { name: "Spinal Twist", img: "images/yoga series/spinal-twist.PNG"},
+      { name: "Triangle Pose", img: "images/yoga series/triangle-pose.PNG"},
+      { name: "Shoulderstand", img: "images/yoga series/shoulderstand.PNG"},
+      { name: "Ship Pose", img: "images/yoga series/ship-pose.PNG"},
+    ],
+  },
+  "Cheer Series": {
+    cover: "images/cheer series/dancing.PNG",
+    figures: [
+      { name: "Marching", img: "images/cheer series/marching.PNG"},
+      { name: "On Drums", img: "images/cheer series/on-drums.PNG"},
+      { name: "On Your Side", img: "images/cheer series/on-your-side.PNG"},
+      { name: "Dancing", img: "images/cheer series/dancing.PNG"},
+      { name: "Little Cheerleading", img: "images/cheer series/little-cheerleading.PNG"},
+      { name: "Cheering", img: "images/cheer series/cheering.PNG"},
+    ],
+  },
+  "Museum Series": {
+    cover: "images/museum series/pearl-earring.PNG",
+    figures: [
+      { name: "The Source", img: "images/museum series/the-source.PNG"},
+      { name: "Fuzin & Raijin", img: "images/museum series/fuzin-raijin.PNG"},
+      { name: "Bacchus", img: "images/museum series/bacchus.PNG"},
+      { name: "Vela’zquez", img: "images/museum series/velazquez.PNG"},
+      { name: "Dali", img: "images/museum series/dali.PNG"},
+      { name: "Pearl Earring", img: "images/museum series/pearl-earring.PNG"},
+    ],
+  },
+  "@ Work Series": {
+    cover: "images/@ work series/researching.PNG",
+    figures: [
+      { name: "Approving", img: "images/@ work series/approving.PNG"},
+      { name: "Researching", img: "images/@ work series/researching.PNG"},
+      { name: "Presenting", img: "images/@ work series/presenting.PNG"},
+      { name: "Good Idea", img: "images/@ work series/good-idea.PNG"},
+      { name: "On the Road", img: "images/@ work series/on-the-road.PNG"},
+      { name: "Little Group Think", img: "images/@ work series/little-group-think.PNG"},
+    ],
+  },
+  "Dressing Series": {
+    cover: "images/dressing series/sweater.PNG",
+    figures: [
+      { name: "Underpants", img: "images/dressing series/underpants.PNG"},
+      { name: "Struggling", img: "images/dressing series/struggling.PNG"},
+      { name: "Loose Pants", img: "images/dressing series/loose-pants.PNG"},
+      { name: "Putting On Socks", img: "images/dressing series/putting-on-socks.PNG"},
+      { name: "Sweater", img: "images/dressing series/sweater.PNG"},
+      { name: "Tight Pants", img: "images/dressing series/tight-pants.PNG"},
+    ],
+  },
+  "Exercising Series": {
+    cover: "images/exercising series/hoop.PNG",
+    figures: [
+      { name: "Doing Crunches", img: "images/exercising series/doing-crunches.PNG"},
+      { name: "Aerobics", img: "images/exercising series/aerobics.PNG"},
+      { name: "Little Balance", img: "images/exercising series/little-balance.PNG"},
+      { name: "Dumbbell", img: "images/exercising series/dumbbell.PNG"},
+      { name: "Hoop", img: "images/exercising series/hoop.PNG"},
+      { name: "Stretch", img: "images/exercising series/stretch.PNG"},
+    ],
+  },
+  "Moving Series": {
+    cover: "images/moving series/balancing-boxes.PNG",
+    figures: [
+      { name: "Carrying Ladder", img: "images/moving series/carrying-ladder.PNG"},
+      { name: "Balancing Boxes", img: "images/moving series/balancing-boxes.PNG"},
+      { name: "Decorating", img: "images/moving series/decorating.PNG"},
+      { name: "Little Teamwork", img: "images/moving series/little-teamwork.PNG"},
+      { name: "Green Thumb", img: "images/moving series/green-thumb.PNG"},
+      { name: "Falling Down", img: "images/moving series/falling-down.PNG"},
+    ],
+  },
+  "Hippers Series": {
+    cover: "images/hippers series/looking-out.PNG",
+    figures: [
+      { name: "On His Smartphone", img: "images/hippers series/on-his-smartphone.PNG"},
+      { name: "Trying to Climb", img: "images/hippers series/trying-to-climb.PNG"},
+      { name: "Looking Out", img: "images/hippers series/looking-out.PNG"},
+      { name: "Sticking", img: "images/hippers series/sticking.PNG"},
+      { name: "Dozing", img: "images/hippers series/dozing.PNG"},
+      { name: "Upside Down", img: "images/hippers series/upside-down.PNG"},
+    ],
+  },
+  "Sunday Series": {
+    cover: "images/sunday series/paper-airplane.PNG",
+    figures: [
+      { name: "Blowing Bubbles", img: "images/sunday series/blowing-bubbles.PNG"},
+      { name: "Paper Airplane", img: "images/sunday series/paper-airplane.PNG"},
+      { name: "Sunbathing", img: "images/sunday series/sunbathing.PNG"},
+      { name: "Sing-Along", img: "images/sunday series/sing-along.PNG"},
+      { name: "Skateboarding", img: "images/sunday series/skateboarding.PNG"},
+      { name: "Gardening", img: "images/sunday series/gardening.PNG"},
+    ],
+  },
+  "Birthday Series": {
+    cover: "images/birthday series/birthday-message.PNG",
+    figures: [
+      { name: "Giving a Bouquet", img: "images/birthday series/giving-bouquet.PNG"},
+      { name: "Wrapped Up", img: "images/birthday series/wrapped-up.PNG"},
+      { name: "Popping Confetti", img: "images/birthday series/popping-confetti.PNG"},
+      { name: "Birthday Message", img: "images/birthday series/birthday-message.PNG"},
+      { name: "Little Decorating", img: "images/birthday series/little-decorating.PNG"},
+      { name: "Tasting", img: "images/birthday series/tasting.PNG"},
+    ],
+  },
+};
+
+// ---------------- Homepage Logic ----------------
+if (document.body.classList.contains("home-page")) {
+  const container = document.getElementById("series-container");
+  const search = document.getElementById("search");
+  const clearAllBtn = document.getElementById("clear-all-btn");
+
+  // Render series cards
+  function renderSeries(filter = "") {
+    container.innerHTML = "";
+
+    Object.entries(smiskiData)
+      .filter(([seriesName]) =>
+        seriesName.toLowerCase().includes(filter.toLowerCase())
+      )
+      .forEach(([seriesName, seriesData]) => {
+        const collected = JSON.parse(localStorage.getItem(seriesName)) || [];
+        const total = seriesData.figures.length;
+        const percent = Math.round((collected.length / total) * 100);
+
+        // Wrapper for card + tracker
+        const wrapper = document.createElement("div");
+        wrapper.className = "series-wrapper";
+
+        // Series card (links to series page)
+        const card = document.createElement("a");
+        card.href = `series.html?name=${encodeURIComponent(seriesName)}`;
+        card.className = "series-card";
+        card.innerHTML = `
+          <div class="image-wrapper">
+            <img src="${seriesData.cover}" alt="${seriesName}">
+            <h2 class="series-title">${seriesName}</h2>
+          </div>
+        `;
+
+        // Tracker (progress bar + count)
+        const tracker = document.createElement("div");
+        tracker.className = "series-tracker";
+        tracker.innerHTML = `
+          <div class="progress-bar">
+            <div class="progress-fill" style="width:${percent}%"></div>
+          </div>
+          <span class="progress-count">${collected.length}/${total}</span>
+        `;
+
+        wrapper.appendChild(card);
+        wrapper.appendChild(tracker);
+        container.appendChild(wrapper);
+      });
+
+    updateOverallProgress(); // Update overall tracker after rendering
+  }
+
+  // Overall progress tracker
+  function updateOverallProgress() {
+    let totalCollected = 0;
+    let totalFigures = 0;
+
+    Object.entries(smiskiData).forEach(([seriesName, seriesData]) => {
+      const collected = JSON.parse(localStorage.getItem(seriesName)) || [];
+      totalCollected += collected.length;
+      totalFigures += seriesData.figures.length;
+    });
+
+    const percent = totalFigures > 0 ? Math.round((totalCollected / totalFigures) * 100) : 0;
+    const overallProgress = document.getElementById("overall-progress");
+
+    if (overallProgress) {
+      overallProgress.querySelector(".progress-fill").style.width = percent + "%";
+      overallProgress.querySelector(".progress-count").textContent = `${totalCollected}/${totalFigures}`;
+    }
+  }
+
+  // Initial render
+  renderSeries();
+
+  // Search filter
+  search.addEventListener("input", (e) => {
+    renderSeries(e.target.value);
+  });
+
+  // Clear all button
+  clearAllBtn.addEventListener("click", () => {
+    if (confirm("Are you sure you want to clear ALL collected Smiskis?")) {
+      Object.keys(localStorage).forEach(key => {
+        if (smiskiData[key]) localStorage.removeItem(key);
+      });
+      renderSeries();           // re-render the series cards
+      updateOverallProgress();  // update the overall tracker
+    }
+  });
+}
+
+// ---------------- Series Page Logic ----------------
+if (document.body.classList.contains("series-page")) {
+  const params = new URLSearchParams(window.location.search);
+  const seriesName = params.get("name");
+  const title = document.getElementById("series-title");
+  const container = document.getElementById("smiski-container");
+  const clearBtn = document.getElementById("clear-btn");
+
+  const series = smiskiData[seriesName];
+  let collected = JSON.parse(localStorage.getItem(seriesName)) || [];
+
+  title.textContent = seriesName;
+
+  function renderFigures() {
+    container.innerHTML = "";
+
+    series.figures.forEach(f => {
+      const isCollected = collected.includes(f.name);
+
+      const card = document.createElement("div");
+      card.className = "smiski-card";
+      if (isCollected) card.classList.add("collected");
+
+      card.innerHTML = `
+        <label class="checkbox-wrapper">
+          <input type="checkbox" ${isCollected ? "checked" : ""}>
+          <span class="checkmark"></span>
+        </label>
+        <img src="${f.img}" alt="${f.name}">
+        <p>${f.name}</p>
+      `;
+
+      const checkbox = card.querySelector("input[type='checkbox']");
+      checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+          if (!collected.includes(f.name)) collected.push(f.name);
+          card.classList.add("collected");
+        } else {
+          collected = collected.filter(n => n !== f.name);
+          card.classList.remove("collected");
+        }
+        localStorage.setItem(seriesName, JSON.stringify(collected));
+      });
+
+      container.appendChild(card);
+    });
+  }
+
+  renderFigures();
+
+  clearBtn.addEventListener("click", () => {
+    if (confirm("Clear all collected Smiskis for this series?")) {
+      collected = [];
+      localStorage.removeItem(seriesName);
+      renderFigures();
+    }
+  });
+}
